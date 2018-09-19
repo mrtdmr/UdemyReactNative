aef8941ae134c0f242e21975e8231f9529fd6e8d
murat <muratdemir.pau@gmail.com>
2018-09-19 22:35:58 +0300

    Components

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,10 @@
-/** @format */
+// Import a library to help create a component
+import React from "react";
+import { AppRegistry } from "react-native";
+import Header from "./src/components/header";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create component
+const App = () => <Header />;
 
-AppRegistry.registerComponent(appName, () => App);
+//Render it to the device
+AppRegistry.registerComponent("albums", () => App);
---
+++ albums/src/components/header.js
@@ -0,0 +1,9 @@
+//Import libraries
+import React from "react";
+import { Text } from "react-native";
+
+//Make a component
+const Header = () => <Text>Albums</Text>;
+
+//Make the component available to other parts of the app
+export default Header;
