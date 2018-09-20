27e004184e7c208f81366ac42228acb1937c88bd
murat <muratdemir.pau@gmail.com>
2018-09-20 23:01:08 +0300

    20.09.2018_2301

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,9 +1,31 @@
 //Import libraries
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 //Make a component
-const Header = () => <Text>Albums</Text>;
+const Header = props => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: "#eff0f2",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    paddingTop: 15,
+    elevation: 4,
+    position: "relative"
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 //Make the component available to other parts of the app
 export default Header;
